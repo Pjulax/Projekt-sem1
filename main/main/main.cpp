e78fbf7cd20ccf79c9aa5939c3192be053bd741d
Pjulax1 <pawelpo99@gmail.com>
2018-12-08 19:42:36 +0100

    first try, leaks lib provided

---
+++ main/main/main.cpp
@@ -0,0 +1,17 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include "pch.h"
+#include "vld.h" // memory leak detection
+#include <iostream>
+
+int main()
+{
+	/*
+	int* pHead = new int; // nowy adres zarezerwowany
+	delete pHead;
+	int a = 10; // a = 10 na jakims adresie
+	//pHead = &a; //  ILLEGAL -> zostawia zajêty adres "pHead" i zastêpuje go adresem "a"
+	*/
+
+	return 0;
+}
